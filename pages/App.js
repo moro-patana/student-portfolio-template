a49dcd3791e8460102a77a76167a63a45cd3079f
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-11-28 13:33:04 +0300

    imported RecipeBlog

--- pages/App.js
+++ pages/App.js
@@ -2,6 +2,7 @@ import React from "react"
 import About from "../components/about"
 import '../styles/body.css'
 import FrontEnd from "../components/frontEnd"
+import RecipeBlog from "../components/blog"
 
 function App() {
     return (
@@ -9,6 +10,7 @@ function App() {
             <h1>Students Portfolio</h1>
             <About/>
             <FrontEnd/>
+            <RecipeBlog/>
         </div>
     )
 }
