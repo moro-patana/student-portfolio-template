03b63bb6292e977b118b90eb5bf34c917eb71a38
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-11-28 09:16:34 +0300

    check spalling

--- pages/App.js
+++ pages/App.js
@@ -3,7 +3,7 @@ import About from "../components/about"
 
 function App() {
     return (
-        <div class="container">
+        <div className="container">
             <h1>Students Portfolio</h1>
             <About/>
         </div>
