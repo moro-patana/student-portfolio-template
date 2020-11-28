26a43b34f5965973d9eb6fcc6d443a9f791a846e
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-11-28 10:53:32 +0300

    gave class name to the article and the div

--- components/frontEnd.js
+++ components/frontEnd.js
@@ -2,28 +2,28 @@ import React from 'react'
 
 function FrontEnd(props) {
     return (
-        <article>
-            <div>
+        <article className="progress">
+            <div className="progress-bar">
                 <label for="react">React</label>
                 <progress id="react" max="100" value="70"> 70% </progress>
             </div>
-            <div>
+            <div className="progress-bar">
                 <label for="javascript">Javascript</label>
                 <progress id="javascript" max="100" value="70"> 60% </progress>
             </div>
-            <div>
+            <div className="progress-bar">
                 <label for="css">CSS</label>
                 <progress id="css" max="100" value="70"> 70% </progress>
             </div>
-            <div>
+            <div className="progress-bar">
                 <label for="vue">Vue</label>
                 <progress id="vue" max="100" value="70"> 2% </progress>
             </div>
-            <div>
+            <div className="progress-bar">
                 <label for="redux">Redux</label>
                 <progress id="redux" max="100" value="70"> 10% </progress>
             </div>
-            <div>
+            <div className="progress-bar">
                 <label for="react-native">React Native</label>
                 <progress id="react-native" max="100" value="70"> 30% </progress>
             </div>
