32b7b43375867044acc5c4c488f704f34b46c488
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-11-28 14:50:31 +0300

    imported OrganiseCss and exchanged place of two functions

--- pages/App.js
+++ pages/App.js
@@ -3,14 +3,16 @@ import About from "../components/about"
 import '../styles/body.css'
 import FrontEnd from "../components/frontEnd"
 import RecipeBlog from "../components/blog"
+import OrganiseCss from "../components/css"
 
 function App() {
     return (
         <div className="container">
             <h1>Students Portfolio</h1>
             <About/>
-            <FrontEnd/>
             <RecipeBlog/>
+            <FrontEnd/>
+            <OrganiseCss/>
         </div>
     )
 }
