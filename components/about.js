07f886d608b69644f84bd54060f11286b6070f7e
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-11-28 08:45:39 +0300

    created an article about me

---
+++ components/about.js
@@ -0,0 +1,25 @@
+import React from "react"
+
+function About() {
+    return (
+        <article>
+            <img src="" alt="my profile"/>
+            <div>
+                <div>
+                    <div>
+                        <h3>Name</h3>
+                        <p>Job</p>
+                    </div>
+                    <div>
+                        <p>📩email</p>
+                        <p>📞 Phone</p>
+                    </div>
+                </div>
+                <p>Ever need to create some filler text in your Word document or PowerPoint presentation to test formatting and layout? Microsoft Office can quickly create this dummy text for you with lorem ipsum Latin or random sentences in English. <br></br>
+                Here's how to use the sample text generator.</p>
+            </div>
+        </article>
+
+    )
+}
+export default About;
