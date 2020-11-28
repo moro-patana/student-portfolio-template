59f936e79dc7204eefeced10d3634db0a3d71322
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-11-28 10:47:40 +0300

    created front end file to store all the progress bars

---
+++ components/frontEnd.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+function frontEnd(props) {
+    return (
+        <article>
+            <div>
+                <label for="react">React</label>
+                <progress id="react" max="100" value="70"> 70% </progress>
+            </div>
+            <div>
+                <label for="javascript">Javascript</label>
+                <progress id="javascript" max="100" value="70"> 60% </progress>
+            </div>
+            <div>
+                <label for="css">CSS</label>
+                <progress id="css" max="100" value="70"> 70% </progress>
+            </div>
+            <div>
+                <label for="vue">Vue</label>
+                <progress id="vue" max="100" value="70"> 2% </progress>
+            </div>
+            <div>
+                <label for="redux">Redux</label>
+                <progress id="redux" max="100" value="70"> 10% </progress>
+            </div>
+            <div>
+                <label for="react-native">React Native</label>
+                <progress id="react-native" max="100" value="70"> 30% </progress>
+            </div>
+
+            
+        </article>
+    )
+}
+
+
+export default frontEnd
+
