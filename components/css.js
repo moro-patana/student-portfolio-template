e07755711faf9165bca5bc993911b3b9a83a7902
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-11-28 15:56:43 +0300

    wrapped the p in a div adn removed br element

--- components/css.js
+++ components/css.js
@@ -11,12 +11,15 @@ function OrganiseCss() {
                 <h2>How to organise your css</h2>
             </header>
             <img src={LearnCss} alt="Learn coding css"/>
-            <p>
-            In this project, I work with HTML and CSS to create a responsive page . The design is from devchallenge.io. 
-            <br></br>
-            Donec aliquam est dui, vel vestibulum diam sollicitudin id. Quisque feugiat malesuada molestie. 
-            </p>
-            <span><a href="/dev">dev.to</a></span>
+            <div className="learn__css__method">
+                <p className="learn__css__instruction">
+                In this project, I work with HTML and CSS to create a responsive page . The design is from devchallenge.io. 
+                </p>
+                <p>
+                Donec aliquam est dui, vel vestibulum diam sollicitudin id. Quisque feugiat malesuada molestie. 
+                </p>
+                <span><a href="/dev">dev.to</a></span>
+            </div>
         </article>
     )
 }
