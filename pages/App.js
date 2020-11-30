6dd6b51cac82c488cc4e02772900f1307f722b77
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-11-30 08:01:31 +0300

    used Switch and Route to wrapp the imported function

--- pages/App.js
+++ pages/App.js
@@ -1,19 +1,37 @@
-import React from "react"
-import About from "../components/about"
-import '../styles/body.css'
-import FrontEnd from "../components/frontEnd"
-import RecipeBlog from "../components/blog"
-import OrganiseCss from "../components/css"
+// import React from "react"
+// import About from "../components/about"
+// import '../styles/body.css'
+// import FrontEnd from "../components/frontEnd"
+// import RecipeBlog from "../components/blog"
+// import OrganiseCss from "../components/css"
 
+// function App() {
+//     return (
+//         <div className="container">
+//             <h1>Students Portfolio</h1>
+//             <About/>
+//             <RecipeBlog/>
+//             <FrontEnd/>
+//             <OrganiseCss/>
+//         </div>
+//     )
+// }
+// export default App;
+import React from "react"
+import { Switch, Route } from "react-router-dom"
+import '../styles/body.css'
+import About from "../components/about"
+import Responsive from "../components/designList"
 function App() {
-    return (
-        <div className="container">
-            <h1>Students Portfolio</h1>
-            <About/>
-            <RecipeBlog/>
-            <FrontEnd/>
-            <OrganiseCss/>
-        </div>
-    )
+  return (
+      <div className="container">
+          <About/>
+          <Switch>
+              <Route path="/">
+                  <Responsive/>
+              </Route>
+          </Switch>
+      </div>
+  )
 }
 export default App;
